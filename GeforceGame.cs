50b115a29919e6d5d9a143cc9c49f1902a2d5b43
Brandon <brandon_corchado1@hotmail.com>
2020-11-27 23:30:34 -0600

    Add project files.

---
+++ GeforceGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NVIDIAGeForceNowEnabler
+{
+    public class GeforceGame
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string steamUrl { get; set; }
+        public string store { get; set; }
+        
+    }
+}
---
+++ NVIDIAGeForceNowEnabler.cs
@@ -0,0 +1,256 @@
+﻿using Playnite.SDK;
+using Playnite.SDK.Models;
+using Playnite.SDK.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace NVIDIAGeForceNowEnabler
+{
+    public class NVIDIAGeForceNowEnabler : Plugin
+    {
+        private static readonly ILogger logger = LogManager.GetLogger();
+
+        private NVIDIAGeForceNowEnablerSettings settings { get; set; }
+
+        public override Guid Id { get; } = Guid.Parse("5f2dfd12-5f13-46fe-bcdd-64eb53ace26a");
+
+        public NVIDIAGeForceNowEnabler(IPlayniteAPI api) : base(api)
+        {
+            settings = new NVIDIAGeForceNowEnablerSettings(this);
+        }
+
+        public override void OnGameInstalled(Game game)
+        {
+            // Add code to be executed when game is finished installing.
+        }
+
+        public override void OnGameStarted(Game game)
+        {
+            // Add code to be executed when game is started running.
+        }
+
+        public override void OnGameStarting(Game game)
+        {
+            // Add code to be executed when game is preparing to be started.
+        }
+
+        public override void OnGameStopped(Game game, long elapsedSeconds)
+        {
+            // Add code to be executed when game is preparing to be started.
+        }
+
+        public override void OnGameUninstalled(Game game)
+        {
+            // Add code to be executed when game is uninstalled.
+        }
+
+        public override void OnApplicationStarted()
+        {
+            // Add code to be executed when Playnite is initialized.
+        }
+
+        public override void OnApplicationStopped()
+        {
+            // Add code to be executed when Playnite is shutting down.
+        }
+
+        public override void OnLibraryUpdated()
+        {
+            // Add code to be executed when library is updated.
+        }
+
+        public override ISettings GetSettings(bool firstRunSettings)
+        {
+            return settings;
+        }
+
+        public override UserControl GetSettingsView(bool firstRunSettings)
+        {
+            return new NVIDIAGeForceNowEnablerSettingsView();
+        }
+         
+        public override List<MainMenuItem> GetMainMenuItems(GetMainMenuItemsArgs menuArgs)
+        {
+            return new List<MainMenuItem>
+            {
+                new MainMenuItem
+                {
+                    Description = "Update game features",
+                    MenuSection = "@Nvidia GeForce NOW Enabler",
+                    Action = args => {
+                        MainMethod();
+                    }
+                },
+            };
+        }
+        public void RemoveFeature(Game game, GameFeature feature)
+        {
+            if (game.FeatureIds != null)
+            {
+                if (game.FeatureIds.Contains(feature.Id))
+                {
+                    game.FeatureIds.Remove(feature.Id);
+                    //PlayniteApi.Database.Games.Update(game);
+                    logger.Info(String.Format("NVIDIA GeForce NOW Enabler - Feature removed from {0}", game.Name));
+                }
+            }
+        }
+        public void AddFeature(Game game, GameFeature feature)
+        {
+            if (game.FeatureIds == null)
+            {
+                game.FeatureIds = new List<Guid> { feature.Id };
+                PlayniteApi.Database.Games.Update(game);
+                logger.Info(String.Format("NVIDIA GeForce NOW Enabler - Feature added to {0}", game.Name));
+            }
+            else if (game.FeatureIds.Contains(feature.Id) == false)
+            {
+                game.FeatureIds.AddMissing(feature.Id);
+                PlayniteApi.Database.Games.Update(game);
+                logger.Info(String.Format("NVIDIA GeForce NOW Enabler - Feature added to {0}", game.Name));
+            }
+        }
+        public void AddOtherAction(Game game, GameAction gameAction)
+        {
+            if (game.OtherActions == null)
+            {
+                game.OtherActions = new System.Collections.ObjectModel.ObservableCollection<GameAction> { gameAction };
+            }
+            else
+            {
+                game.OtherActions.AddMissing(gameAction);
+            }
+        }
+
+        public void AddNvidiaAction(Game game, GeforceGame supportedGame, string geforceNowWorkingPath, string geforceNowExecutablePath)
+        {
+            GameAction geforceNowAction = null;
+            if (game.OtherActions != null)
+            {
+                geforceNowAction = game.OtherActions.Where(x => Regex.IsMatch(x.Arguments, "--url-route=\"#?cmsId=100034911&launchSource=External\"")).FirstOrDefault();
+            }
+
+            if (supportedGame == null && geforceNowAction != null)
+            {
+                game.OtherActions.Remove(geforceNowAction);
+                PlayniteApi.Database.Games.Update(game);
+                logger.Info(String.Format("NVIDIA GeForce NOW Enabler - Play Action removed from {0}", game.Name));
+
+            }
+            else if (supportedGame != null && geforceNowAction == null)
+            {
+                GameAction nvidiaGameAction = new GameAction();
+                nvidiaGameAction.Name = "Launch in Nvidia GeForce NOW";
+                var playActionArguments = String.Format("--url-route=\"#?cmsId={0}&launchSource=External`\"", supportedGame.id);
+                nvidiaGameAction.Arguments = playActionArguments;
+                nvidiaGameAction.Path = geforceNowExecutablePath;
+                nvidiaGameAction.WorkingDir = geforceNowWorkingPath;
+
+                AddOtherAction(game, nvidiaGameAction);
+
+                //PlayniteApi.Database.Games.Update(game);
+                logger.Info(String.Format("NVIDIA GeForce NOW Enabler - Play Action added to {0}", game.Name));
+            }
+        }
+        public List<GeforceGame> DownloadGameList(string uri)
+        {
+            List<GeforceGame> supportedGames = new List<GeforceGame>();
+
+            try
+            {
+                WebClient webClient = new WebClient();
+                string downloadedString = webClient.DownloadString(uri);
+                supportedGames = JsonConvert.DeserializeObject<List<GeforceGame>>(downloadedString);
+                foreach (var supportedGame in supportedGames)
+                {
+                    supportedGame.title = Regex.Replace(supportedGame.title, "[^\\p{L}\\p{Nd}]", "").ToLower();
+                }
+                return supportedGames;
+            }
+            catch (Exception e)
+            {
+                logger.Error(e, e.Message);
+                PlayniteApi.Dialogs.ShowErrorMessage(e.Message, "ERROR");
+                return supportedGames;
+            }
+        }
+
+        public void MainMethod()
+        {
+
+            GameFeature feature = PlayniteApi.Database.Features.Add("My Test Feature...");
+
+            bool updatePlayActions = true;
+
+            string localAppData = Environment.GetEnvironmentVariable("LocalAppData");
+            string[] paths = { localAppData, "NVIDIA Corporation", "GeForceNOW", "CEF" };
+            string geforceNowWorkingPath = Path.Combine(paths);
+            string geforceNowExecutablePath = geforceNowWorkingPath + "\\GeForceNOWStreamer.exe";
+                        
+            var supportedGames = DownloadGameList("https://static.nvidiagrid.net/supported-public-game-list/gfnpc.json");
+            if (supportedGames.Count() == 0)
+            {
+                return;
+            }
+            var supportedSteamGames = supportedGames.Where(g => g.store == "Steam");
+            var supportedEpicGames = supportedGames.Where(g => g.store == "Epic");
+            var supportedOriginGames = supportedGames.Where(g => g.store == "Origin");
+            var supportedUplayGames = supportedGames.Where(g => g.store == "UPLAY");
+
+            List<Guid> supportedLibraries = new List<Guid>() {
+                BuiltinExtensions.GetIdFromExtension(BuiltinExtension.EpicLibrary),
+                BuiltinExtensions.GetIdFromExtension(BuiltinExtension.OriginLibrary),
+                BuiltinExtensions.GetIdFromExtension(BuiltinExtension.SteamLibrary),
+                BuiltinExtensions.GetIdFromExtension(BuiltinExtension.UplayLibrary)
+            };
+
+            var gameDatabase = PlayniteApi.Database.Games.Where(g => supportedLibraries.Contains(g.PluginId));
+            foreach (var game in gameDatabase)
+            {
+                var gameName = Regex.Replace(game.Name, "[^\\p{L}\\p{Nd}]", "").ToLower();
+                GeforceGame supportedGame = null;
+                switch (game.PluginId)
+                {
+                    case var id when BuiltinExtensions.GetExtensionFromId(id) == BuiltinExtension.SteamLibrary:
+                        var steamUrl = String.Format("https://store.steampowered.com/app/{0}", game.GameId);
+                        supportedGame = supportedSteamGames.Where(g => g.steamUrl == steamUrl).FirstOrDefault();
+                        break;
+                    case var id when BuiltinExtensions.GetExtensionFromId(id) == BuiltinExtension.EpicLibrary:
+                        supportedGame = supportedEpicGames.Where(g => g.title == gameName).FirstOrDefault();
+                        break;
+                    case var id when BuiltinExtensions.GetExtensionFromId(id) == BuiltinExtension.OriginLibrary:
+                        supportedGame = supportedOriginGames.Where(g => g.title == gameName).FirstOrDefault();
+                        break;
+                    case var id when BuiltinExtensions.GetExtensionFromId(id) == BuiltinExtension.UplayLibrary:
+                        supportedGame = supportedUplayGames.Where(g => g.title == gameName).FirstOrDefault();
+                        break;
+                    default:
+                        break;
+                }
+
+                if (supportedGame == null)
+                {
+                    RemoveFeature(game, feature);
+                }
+                
+                if (supportedGame != null)
+                {
+                    AddFeature(game, feature);
+                }
+
+                if (updatePlayActions == true)
+                {
+                    AddNvidiaAction(game, supportedGame, geforceNowWorkingPath, geforceNowExecutablePath);
+                }
+            }
+        }
+    }
+}
