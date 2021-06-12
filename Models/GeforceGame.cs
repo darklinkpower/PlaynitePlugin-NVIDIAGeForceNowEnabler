b20b35af63f9c15e346075807bce1fccae22d64d
Brandon <brandon_corchado1@hotmail.com>
2021-06-12 17:34:21 -0500

    Improvements to GeforceGame model

--- Models/GeforceGame.cs
+++ Models/GeforceGame.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,32 @@ namespace NVIDIAGeForceNowEnabler
         public string title { get; set; }
         public string steamUrl { get; set; }
         public string store { get; set; }
-        
+
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("sortName")]
+        public string SortName { get; set; }
+
+        [JsonProperty("isFullyOptimized")]
+        public bool IsFullyOptimized { get; set; }
+
+        [JsonProperty("steamUrl")]
+        public string SteamUrl { get; set; }
+
+        [JsonProperty("store")]
+        public string Store { get; set; }
+
+        [JsonProperty("publisher")]
+        public string Publisher { get; set; }
+
+        [JsonProperty("genres")]
+        public string[] Genres { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
     }
 }
